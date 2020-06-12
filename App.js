9edaf43ba88c18a6f2bc46374bb5dfc860b5dc51
Alexandre Finger <alexandre.finger@esales.com.br>
2020-06-12 08:03:53 -0300

    Implemented Firebase cloud storage upload  (from local storage only)

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import {
-  SafeAreaView,
   View,
   Text,
   TextInput,
   StyleSheet,
   Button,
+  ImageBackground,
+  Image,
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import { LoginManager, AccessToken } from 'react-native-fbsdk';
+import SocialIcon from './src/components/SocialIcon';
+import Main from './src/main';
 
 const App = () => {
-  // Set an initializing state whilst Firebase connects
+  const image = require('./src/assets/login-background.png');
   const [user, setUser] = useState();
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -26,18 +28,44 @@ const App = () => {
       .catch((error) => console.log(error));
   };
 
-  const FacebookSignIn = () => {
-    return (
-      <Button
-        title="Facebook Sign-In"
-        onPress={() => onFacebookButtonPress().then(() => console.log('Signed in with Facebook!'))}
-      />
-    );
-  }
+  const handleFacebookLogin = () => {
+    console.log('Facebook login clicked');
+    facebookLogin().then((userCredential) => {
+      console.log('userCredential', userCredential);
+      console.log('additionalUserInfo', userCredential.additionalUserInfo);
+      console.log('profile', userCredential.additionalUserInfo.profile);
+      console.log('user', userCredential.additionalUserInfo.user);
+      setUser(userCredential.additionalUserInfo.profile);
+    });
+  };
 
-  const onFacebookButtonPress = async () => {
+  const handleGoogleLogin = () => {
+    console.log('Google login not yet implemented!');
+    // facebookLogin().then((userCredential) => {
+    //   console.log('userCredential', userCredential);
+    // });
+  };
+
+  const handleTwitterLogin = () => {
+    console.log('Twitter login not yet implemented!');
+    // facebookLogin().then((userCredential) => {
+    //   console.log('userCredential', userCredential);
+    // });
+  };
+
+  const handleGitHubLogin = () => {
+    console.log('GitHub login not yet implemented!');
+    // facebookLogin().then((userCredential) => {
+    //   console.log('userCredential', userCredential);
+    // });
+  };
+
+  const facebookLogin = async () => {
     // Attempt login with permissions
-    const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+    const result = await LoginManager.logInWithPermissions([
+      'public_profile',
+      'email',
+    ]);
 
     if (result.isCancelled) {
       throw 'User cancelled the login process';
@@ -51,77 +79,159 @@ const App = () => {
     }
 
     // Create a Firebase credential with the AccessToken
-    const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+    const facebookCredential = auth.FacebookAuthProvider.credential(
+      data.accessToken
+    );
 
     // Sign-in the user with the credential
     return auth().signInWithCredential(facebookCredential);
-  }
+  };
 
   console.log('user is ', user);
 
+
   if (!user) {
     return (
-      <SafeAreaView>
-        <View style={styles.form}>
-          <Text style={styles.defaultText}>Welcome to Rogue One</Text>
-          <TextInput
-            style={styles.input}
-            placeholder='Enter you e-mail'
-            value={email}
-            onChangeText={(inputValue) => setEmail(inputValue)}
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('./src/assets/nuvem.png')}
+            style={styles.image}
+            // resizeMode="cover"
           />
-          <TextInput
-            style={styles.input}
-            placeholder='Enter password'
-            value={password}
-            onChangeText={(inputValue) => setPassword(inputValue)}
-          />
-          <Button
-            title='Login'
-            color='#C2185B'
-            onPress={handleEmailPasswordLogin}
-          />
-          <FacebookSignIn/>
         </View>
-      </SafeAreaView>
+        {/* <ImageBackground source={image} style={styles.backgroundImage}> */}
+        <View style={styles.formContainer}>
+          <View style={styles.loginForm}>
+            <Text style={styles.defaultText}>Welcome to Rogue One</Text>
+            <TextInput
+              style={styles.input}
+              placeholder='Enter you e-mail'
+              value={email}
+              onChangeText={(inputValue) => setEmail(inputValue)}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder='Enter password'
+              value={password}
+              onChangeText={(inputValue) => setPassword(inputValue)}
+            />
+          </View>
+          <View style={styles.actionContainer}>
+            <Button
+              style={styles.button}
+              title='Login'
+              color='#C2185B'
+              onPress={handleEmailPasswordLogin}
+            />
+          </View>
+          <View style={styles.socialContainer}>
+            <SocialIcon
+              type='facebook'
+              style={{ color: '#3b5998' }}
+              handleOnPress={handleFacebookLogin}
+            />
+            <SocialIcon
+              type='google'
+              style={{ color: '#3b5998' }}
+              handleOnPress={handleGoogleLogin}
+            />
+            <SocialIcon
+              type='twitter'
+              style={{ color: '#3b5998' }}
+              handleOnPress={handleTwitterLogin}
+            />
+            <SocialIcon
+              type='github'
+              style={{ color: '#3b5998' }}
+              handleOnPress={handleGitHubLogin}
+            />
+          </View>
+        </View>
+        {/* </ImageBackground> */}
+      </View>
     );
   }
 
-  return (
-    <View>
-      <Text>Welcome {user.email}</Text>
-    </View>
-  );
+  return <Main user={user} />;
 };
 
 const styles = StyleSheet.create({
-  form: {
+  container: {
+    flex: 1,
     flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    marginTop: '60%',
-    margin: 20,
-    padding: 10,
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white',
-    height: 200,
+    // justifyContent: 'space-around',
+    // alignItems: 'center',
+
   },
+  imageContainer: {
+    flex: 2,
+    // alignItems: 'center'
+
+  },
+  formContainer: {
+    flex: 2,
+    backgroundColor: '#DEDEDE',
+    alignItems: 'center',
+    // justifyContent: 'center'
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    // opacity: 0.1,
+  },
+  // loginForm: {
+    // top: 0,
+    // bottom: 0,
+    // left: 0,
+    // right: 0,
+    // margin: 10,
+    // padding: 5,
+  // },
   input: {
-    borderColor: 'cyan',
-    borderWidth: 1,
-    width: '100%',
-    height: 40,
+    height: 50,
     margin: 10,
     fontSize: 18,
-    paddingLeft: 5,
+    backgroundColor: 'rgba(255,255,255,0.6)',
+    borderRadius: 50,
+    borderWidth: 0,
+    padding: 15,
   },
   defaultText: {
-    fontSize: 18,
+    color: '#939',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  actionContainer: {
+    flexDirection: 'row',
+    // justifyContent: 'space-between',
+    // alignContent: 'center',
+    marginTop: 15,
+    // marginLeft: 15,
+    // paddingHorizontal: 25,
+  },
+  socialContainer: {
+    // flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    // backgroundColor: 'red',
+    width: '70%',
+    marginTop: 20,
+    // alignContent: 'center',
+    // alignItems: 'center',
+  },
+  image: {
+    flex: 1,
+    // flex: 3,
+    width: '100%',
+    // height: 19,
+    // paddingTop: 25,
+
+    // opacity: 0.5,
+    backgroundColor: '#DEDEDE',
   },
 });
 
